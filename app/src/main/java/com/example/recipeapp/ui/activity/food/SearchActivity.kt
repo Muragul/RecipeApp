8bcef435c5902dba410c620eeb0e2f04e2d80026
Muratbekova Muragul <47049297+Muragul@users.noreply.github.com>
2020-12-18 22:00:03 +0600

    added search menu

--- app/src/main/java/com/example/recipeapp/ui/activity/food/SearchActivity.kt
+++ app/src/main/java/com/example/recipeapp/ui/activity/food/SearchActivity.kt
@@ -1,8 +1,11 @@
 package com.example.recipeapp.ui.activity.food
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.PopupMenu
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.FragmentManager
 import com.example.recipeapp.R
 import com.example.recipeapp.ui.fragment.food.SearchFragment
@@ -15,11 +18,50 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         fragmentManager = supportFragmentManager
+        back.setOnClickListener {
+            onBackPressed()
+        }
+        val popupMenu = PopupMenu(this, open_menu)
+        popupMenu.inflate(R.menu.popup_menu)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu1 -> {
+                    fragmentManager.beginTransaction()
+                        .replace(R.id.fragment, SearchFragment("avocado"))
+                        .commit()
+                    preview_text.text = ""
+                    true
+                }
+                R.id.menu2 -> {
+                    fragmentManager.beginTransaction()
+                        .replace(R.id.fragment, SearchFragment("vanilla"))
+                        .commit()
+                    preview_text.text = ""
+                    true
+                }
+                R.id.menu3 -> {
+                    fragmentManager.beginTransaction()
+                        .replace(R.id.fragment, SearchFragment("basil"))
+                        .commit()
+                    preview_text.text = ""
+                    true
+                }
+                else -> false
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            popupMenu.setForceShowIcon(true)
+        }
+        open_menu.setOnClickListener {
+            popupMenu.show()
+        }
 
         search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 search_view.clearFocus()
-                fragmentManager.beginTransaction().replace(R.id.fragment, SearchFragment(query)).commit()
+                fragmentManager.beginTransaction().replace(R.id.fragment, SearchFragment(query))
+                    .commit()
+                preview_text.text = ""
                 return false
             }
 
